b35c69745421fa75100c533f9e8abcc470b9fa12
Hashaam76 <hashamaamir019@gmail.com>
2025-04-23 16:47:38 +0500

    Javascript by Hashaam

---
+++ Javascript/index.js
@@ -0,0 +1,13 @@
+var name = "Hashaam";
+var city = "Faisalabad";
+var rollNum = "1234";
+var newRollNum = rollNum + 23;
+var phoneNum = 923011234556;
+var num = 13 % 3;
+// var num = num +1;
+var num = ++num; 
+var num = num--;
+
+var total = 2 * (3 + 19);
+var msg = "Your form is submitted";
+alert(msg);
